124d37dcdcf850944f6585e5ef9b6e6568eb4f28
Aman Agarwal``````````````````````````````````````` <amana0256@gmail.com>
2021-10-09 14:58:07 +0530

    one more file

---
+++ sub.java
@@ -0,0 +1,9 @@
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+public class sub {
+    public static void main(String[] args) {
+        int n1 = 220 ,n2 = 210;
+        int sub = n1- n2;
+        System.out.println("The su of numbers is  : "+sub);
+    }
+}
